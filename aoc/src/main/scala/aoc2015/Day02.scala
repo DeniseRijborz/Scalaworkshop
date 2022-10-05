d9d3cbb84bada397e76c6e9cf1da075bce1357f0
Denise Rijborz <denise.rijborz@dhl.com>
2022-10-05 14:10:42 +0200

    Start day2

---
+++ aoc/src/main/scala/aoc2015/Day02.scala
@@ -0,0 +1,14 @@
+package aoc2015
+
+import scala.io.Source
+
+object Day02 extends App:
+
+  val lines: List[String] = Source.fromFile("aoc/src/resources/aoc2015/Day02").getLines.toList
+
+  def test(list: List[String]): List[List[String]] =
+    val newlist = list.map(_.split("x").toList)
+    newlist
+
+  println(lines)
+  println(test(lines))
