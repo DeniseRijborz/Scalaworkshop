c609202e8baac09cda5fd644319c1bdd4f3b66f5
Denise Rijborz <denise.rijborz@dhl.com>
2022-10-06 13:48:11 +0200

    Finish day02

--- aoc/src/main/scala/aoc2015/Day02.scala
+++ aoc/src/main/scala/aoc2015/Day02.scala
@@ -4,12 +4,43 @@ import scala.io.Source
 
 object Day02 extends App:
 
-  val lines: List[String] = Source.fromFile("aoc/src/resources/aoc2015/Day02").getLines.toList
+  val lines: List[String] =
+    Source
+      .fromFile("aoc/src/resources/aoc2015/Day02")
+      .getLines
+      .toList
 
-  def test(list: List[String]): List[List[String]] =
-    val newlist = list.map(_.split("x").toList)
-    newlist
+  def amountofpaper(dimensions: List[String]): Int =
+    dimensions
+      .map(_.split("x").toList)
+      .map(dimension => {
+        val l = dimension(0).toInt
+        val w = dimension(1).toInt
+        val h = dimension(2).toInt
+        val lw = l*w
+        val wh = w*h
+        val hl = h*l
+        val calc = (2*lw + 2*wh + 2*hl) + (lw min wh min hl)
+        calc})
+      .reduce((a,b) => a+b)
+
+  def amountofribbon(dimensions: List[String]): Int =
+    dimensions
+      .map(_.split("x").toList)
+      .map(dimension => {
+        val l = dimension(0).toInt
+        val w = dimension(1).toInt
+        val h = dimension(2).toInt
+        val lwh = l * w * h
+        val min = l min w min h
+        val max = l max w max h
+        val med = (l + w + h) - min - max
+        val calc = (min + min + med + med) + lwh
+        calc
+      })
+      .reduce((a, b) => a + b)
+
 
   println(lines)
-  println(test(lines))
-  println("Hi")
+  println(amountofpaper(lines))
+  println(amountofribbon(lines))
