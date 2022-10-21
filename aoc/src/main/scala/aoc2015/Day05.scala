a727c7c8ec75efd1779588352acb00b14c065e6f
Denise Rijborz <denise.rijborz@dhl.com>
2022-10-21 14:19:59 +0200

    got the right answer

--- aoc/src/main/scala/aoc2015/Day05.scala
+++ aoc/src/main/scala/aoc2015/Day05.scala
@@ -1,6 +1,5 @@
 package aoc2015
 
-import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
 object Day05 extends App:
@@ -11,30 +10,48 @@ object Day05 extends App:
       .getLines
       .toList
 
+  def double(x: String): Boolean =
+    if x.slice(0, 1) == x.slice(1, 2) then true
+    else false
+
+  def triple(x: String): Boolean =
+    if x.slice(0, 1) == x.slice(2, 3) then true
+    else false
+
   def containsThreeVowels(s: String): Boolean =
     val vowels = Set('a', 'e', 'i', 'o', 'u')
     val count = s.toList.count(vowels)
-    if count > 3 then true
-    else false
-
-  def double(x: String): Boolean =
-    if x.slice(0,1) == x.substring(1,2) then true
+    if count >= 3 then true
     else false
 
   def containsLetterTwice(s: String): Boolean =
     if s.sliding(2).toList.map(x => double(x)).contains(true) then true
     else false
 
-  def containsString(s: String): Boolean =
+  def doesNotContainString(s: String): Boolean =
     !s.contains("ab") && !s.contains("cd") && !s.contains("pq") && !s.contains("xy")
 
-  def checkString(file: List[String]): List[((Boolean, Boolean), Boolean)] =
-    val one = file.map(s => containsThreeVowels(s))
-    val two = file.map(s => containsLetterTwice(s))
-    val three = file.map(s => containsString(s))
-    val test = one zip two zip three
-    test
+  def containsPairTwice(s: String): Boolean =
+    val pairs = s.sliding(2).toList.filter(_.length > 1)
+    if pairs.groupBy(identity).filter(_._2.length > 1).nonEmpty then
+      val letters: String = pairs.groupBy(identity).filter(_._2.length > 1).keys.head
+      val index1: Int = s.indexOf(letters)
+      val index2: Int = s.lastIndexOf(letters)
+      if index1 + 1 == index2 then false
+      else true
+    else false
 
-  println(lines)
-  println(lines.head)
+  def containsRepeatLetter(s: String): Boolean =
+    if s.sliding(3).toList.map(x => triple(x)).contains(true) then true
+    else false
+
+  def checkString(file: List[String]): Int =
+    val nice = file.map(s => containsThreeVowels(s) && containsLetterTwice(s) && doesNotContainString(s))
+    nice.count(_ == true)
+
+  def checkString2(file: List[String]): Int =
+    val nice = file.map(s => containsPairTwice(s) && containsRepeatLetter(s))
+    nice.count(_ == true)
+
   println(checkString(lines))
+  println(checkString2(lines))
