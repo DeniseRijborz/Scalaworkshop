a99e78f5e50743d6b0bc9fdf3772ddf673b64a37
Denise Rijborz <denise.rijborz@dhl.com>
2022-10-27 11:15:45 +0200

    add for comprehension

--- aoc/src/main/scala/aoc2015/Day05.scala
+++ aoc/src/main/scala/aoc2015/Day05.scala
@@ -10,26 +10,22 @@ object Day05 extends App:
       .getLines
       .toList
 
-  def double(x: String): Boolean =
+  def doubleLetterInRow(x: String): Boolean =
     if x.slice(0, 1) == x.slice(1, 2) then true
     else false
 
-  def triple(x: String): Boolean =
+  val answer1: Int =
+    val nice = for {
+      s <- lines
+      if s.toList.count("aeiou".toList.contains) >= 3
+      if s.sliding(2).toList.map(x => doubleLetterInRow(x)).contains(true)
+      if !s.contains("ab") && !s.contains("cd") && !s.contains("pq") && !s.contains("xy")
+    } yield s
+    nice.size
+
+  def repeatLetterWithLetterInBetween(x: String): Boolean =
     if x.slice(0, 1) == x.slice(2, 3) then true
     else false
-
-  def containsThreeVowels(s: String): Boolean =
-    val vowels = Set('a', 'e', 'i', 'o', 'u')
-    val count = s.toList.count(vowels)
-    if count >= 3 then true
-    else false
-
-  def containsLetterTwice(s: String): Boolean =
-    if s.sliding(2).toList.map(x => double(x)).contains(true) then true
-    else false
-
-  def doesNotContainString(s: String): Boolean =
-    !s.contains("ab") && !s.contains("cd") && !s.contains("pq") && !s.contains("xy")
 
   def containsPairTwice(s: String): Boolean =
     val pairs = s.sliding(2).toList.filter(_.length > 1)
@@ -42,16 +38,12 @@ object Day05 extends App:
     else false
 
   def containsRepeatLetter(s: String): Boolean =
-    if s.sliding(3).toList.map(x => triple(x)).contains(true) then true
+    if s.sliding(3).toList.map(x => repeatLetterWithLetterInBetween(x)).contains(true) then true
     else false
 
-  def checkString(file: List[String]): Int =
-    val nice = file.map(s => containsThreeVowels(s) && containsLetterTwice(s) && doesNotContainString(s))
-    nice.count(_ == true)
-
-  def checkString2(file: List[String]): Int =
+  def answer2(file: List[String]): Int =
     val nice = file.map(s => containsPairTwice(s) && containsRepeatLetter(s))
     nice.count(_ == true)
 
-  println(checkString(lines))
-  println(checkString2(lines))
+  println(answer1)
+  println(answer2(lines))
